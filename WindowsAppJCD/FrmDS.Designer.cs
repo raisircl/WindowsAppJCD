59dcb4352561e1d4c2da5679bef340b74dc7d777
raisircl <rai.verma@live.com>
2023-09-04 10:35:51 +0530

    dataset work complete

---
+++ WindowsAppJCD/FrmDS.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsAppJCD
+{
+    partial class FrmDS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDNo = new System.Windows.Forms.TextBox();
+            this.txtDName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLoc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.dgvdept = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdept)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DNo";
+            // 
+            // txtDNo
+            // 
+            this.txtDNo.Location = new System.Drawing.Point(144, 38);
+            this.txtDNo.Name = "txtDNo";
+            this.txtDNo.Size = new System.Drawing.Size(123, 41);
+            this.txtDNo.TabIndex = 1;
+            // 
+            // txtDName
+            // 
+            this.txtDName.Location = new System.Drawing.Point(144, 95);
+            this.txtDName.Name = "txtDName";
+            this.txtDName.Size = new System.Drawing.Size(203, 41);
+            this.txtDName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DName";
+            // 
+            // txtLoc
+            // 
+            this.txtLoc.Location = new System.Drawing.Point(144, 159);
+            this.txtLoc.Name = "txtLoc";
+            this.txtLoc.Size = new System.Drawing.Size(123, 41);
+            this.txtLoc.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 164);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 36);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Loc";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(48, 229);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(131, 55);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(185, 229);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(131, 55);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(322, 229);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(131, 55);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(459, 229);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(131, 55);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // dgvdept
+            // 
+            this.dgvdept.AllowUserToAddRows = false;
+            this.dgvdept.AllowUserToDeleteRows = false;
+            this.dgvdept.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvdept.Location = new System.Drawing.Point(48, 314);
+            this.dgvdept.Name = "dgvdept";
+            this.dgvdept.ReadOnly = true;
+            this.dgvdept.RowHeadersWidth = 51;
+            this.dgvdept.RowTemplate.Height = 24;
+            this.dgvdept.Size = new System.Drawing.Size(639, 290);
+            this.dgvdept.TabIndex = 10;
+            this.dgvdept.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvdept_CellClick);
+            // 
+            // FrmDS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 688);
+            this.Controls.Add(this.dgvdept);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtLoc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDNo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.8F);
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "FrmDS";
+            this.Text = "FrmDS";
+            this.Load += new System.EventHandler(this.FrmDS_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdept)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDNo;
+        private System.Windows.Forms.TextBox txtDName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.DataGridView dgvdept;
+    }
+}
---
+++ WindowsAppJCD/FrmDS.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsAppJCD
+{
+    public partial class FrmDS : Form
+    {
+        public FrmDS()
+        {
+            InitializeComponent();
+        }
+        DataSet ds = new DataSet(); // ds is collection of tables in xml form
+        DataTable dt = new DataTable("tbldept");
+        DataColumn dc1 = new DataColumn("DNo");
+        DataColumn dc2 = new DataColumn("DName");
+        DataColumn dc3 = new DataColumn("Loc");
+
+        int cri = -1;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            DataRow dr = ds.Tables["tbldept"].NewRow();
+            dr["DNo"]=txtDNo.Text;
+            dr["DName"] = txtDName.Text;
+            dr["Loc"] = txtLoc.Text;
+            ds.Tables["tbldept"].Rows.Add(dr);
+            reset();
+        }
+
+        private void FrmDS_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add(dc1);
+            dt.Columns.Add(dc2);
+            dt.Columns.Add(dc3);
+            ds.Tables.Add(dt);
+            dgvdept.DataSource = ds.Tables["tbldept"];    
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (cri > -1)
+            {
+                ds.Tables["tbldept"].Rows[cri]["DNo"] = txtDNo.Text;
+                ds.Tables["tbldept"].Rows[cri]["DName"] = txtDName.Text;
+                ds.Tables["tbldept"].Rows[cri]["Loc"] = txtLoc.Text;
+                MessageBox.Show("Record Updated");
+                cri = -1;
+                reset();
+            }
+        }
+
+        private void dgvdept_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex>-1)
+            {
+
+                txtDNo.Text = dgvdept.Rows[e.RowIndex].Cells["DNo"].Value.ToString();
+                txtDName.Text = dgvdept.Rows[e.RowIndex].Cells["DName"].Value.ToString();
+                txtLoc.Text = dgvdept.Rows[e.RowIndex].Cells["Loc"].Value.ToString();
+                cri = e.RowIndex;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(cri > -1)
+            {
+                ds.Tables["tbldept"].Rows[cri].Delete();
+                cri = -1;
+                reset();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+        void reset()
+        {
+            txtDNo.Text = "";
+            txtDName.Text = "";
+            txtLoc.Text = "";
+            txtDNo.Focus();
+        }
+    }
+}
